8e2761b523ed747a96ea28ea788b96f071f10d7d
illia <macbook@MacBook-Neo-illia.local>
2026-04-07 00:00:24 +0200

    feat: add Google Analytics support via NEXT_PUBLIC_GA_ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,31 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
             ]
           })
         }} />
+        
+        {/* Google Analytics */}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+
         {children}
       </body>
     </html>
